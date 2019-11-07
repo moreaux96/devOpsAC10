6f59cb8d8b04da20cbb6e8a50a4df0199c836ad8
Lucas Rabelo Moreaux <1601076@SALAS.AULAS>
2019-11-07 20:10:40 -0300

    subindo prints e modificanod imagens

--- com/faculdadeimpacta/calculadora/app_web_start.py
+++ com/faculdadeimpacta/calculadora/app_web_start.py
@@ -1,17 +1,14 @@
-import os
+"""arquivo"""
 from flask import Flask
-from flask_cors import CORS
 
-app = Flask(__name__)
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
+APP = Flask(__name__)
 
-@app.route('/')
+
+@APP.route('/')
 def index():
+    """funcao index"""
     return 'Index Page'
 
-def main():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
-    main()
+    APP.run(host='0.0.0.0')
